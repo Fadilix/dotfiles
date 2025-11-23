f73a20a89a89f7905ac01da6d1d34b0ddea31ee2
Fadilix <iamfadile2@gmail.com>
2025-11-23 17:12:56 +0100

    feat: added script for app setup

---
+++ setup-scripts/setup-apps.js
@@ -0,0 +1,41 @@
+import { spawn } from "node:child_process";
+
+const packages = [
+  // code
+  "visual-studio-code-bin",
+  "zen-browser-bin",
+  "jetbrains-toolbox",
+  "neovim",
+
+  // linux
+  "vi",
+  "vim",
+  "more",
+  "less",
+
+  // hyprland plugins
+  "hyprsunset",
+  "pyprland",
+];
+
+const installPackages = async (packages) => {
+  for (let pkg of packages) {
+    const installer = spawn("yay", ["-S", pkg, "--noconfirm"], {
+      stdio: "inherit",
+    });
+
+    await new Promise((resolve, reject) => {
+      installer.on("close", (code) => {
+        if (code === 0) {
+          console.log(`${pkg} Installed successfully`);
+          resolve();
+        } else {
+          console.log(`Failed to install ${pkg} (exit code ${code}`);
+          resolve();
+        }
+      });
+    });
+  }
+};
+
+installPackages(packages);
